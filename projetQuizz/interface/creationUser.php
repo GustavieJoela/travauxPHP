d7390106a73a7c87e62dfb86e2e0700efb433c30
GustavieJoela <gustaviedjobouya@gmail.com>
2020-05-07 12:42:33 +0000

    ecrans

---
+++ projetQuizz/interface/creationUser.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title> Création user</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+  <div class=" row">
+        
+        <div class="col-2 ">
+            
+        </div>
+
+        <div class="col-8">
+            <div class="row bg-dark" style="height: 80px;">
+                <div class="col-2">
+                    
+                </div>
+                <div class="col-8">
+                   <p class="text-white  text-center font-weight-bold text-white" style="margin-top:20px; font-size:1.7em;"> Le plaisir de jouer </p> 
+                </div>
+                
+            </div>
+            <div class="row" style=" height:800px; background-image: url('./../Images/img-bg.jpg'); background-size: cover;">
+
+                <div class="col-1">
+                    
+                </div>
+                <div class="col-10 rounded-lg" style="height:99%; margin:auto; background-color:white;">
+                    <div class="row">
+                        <div class="col-7">
+                            <div class=" border-bottom " style="margin 0;">
+                                <h2 class="font-weight-bold"> S'INSCRIRE </h2>
+                                <p> Pour tester votre niveau de culture général </p>
+                            </div>
+                            <form>
+
+                                <div class="form-group">
+                                    <label for="" style="color:#a9a9a9;" > Prénom </label>
+                                    <input type="text" name="" id=""  style="height:55px; " class="form-control" placeholder=" Aaa " aria-describedby="helpId">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="" style="color:#a9a9a9;"> Nom </label>
+                                    <input type="text" name="" id=""  style="height:55px; " class="form-control" placeholder=" BBB " aria-describedby="helpId">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="" style="color:#a9a9a9;"> Login </label>
+                                    <input type="text" name="" id="" style="height:55px; "  class="form-control" placeholder=" aabaab" aria-describedby="helpId">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="" style="color:#a9a9a9;"> password </label>
+                                    <input type="text" name="" id=""  style="height:55px; " class="form-control" placeholder=" ......." aria-describedby="helpId">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="" style="color:#a9a9a9;"> cconfirmer password</label>
+                                    <input type="text" name="" id="" style="height:55px; " class="form-control" placeholder=" ......" aria-describedby="helpId">
+                                </div>
+
+                                <div class="d-inline">
+                                    <span> Avatar </span>
+                                    <a>
+                                        <p style="background-color:#48d1cc; color:#f5f5f5;" > Choisir un fichier</p>
+                                    </a>
+                                </div>
+                                
+                                <button type="submit" class="btn " style="background-color:#48d1cc; color:#f5f5f5;"> Connexion </button>
+                            
+                                
+                            </form>
+                        </div>
+
+                        <div class="col-5">
+                            
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-1">
+                    
+                </div>
+
+                
+            </div>
+            
+        </div>
+
+        <div class="col-2">
+            
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
